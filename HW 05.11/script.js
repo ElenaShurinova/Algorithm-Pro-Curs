758cbac2164cd939323a030233da7f4277663356
ElenaShurinova <166155974+ElenaShurinova@users.noreply.github.com>
2024-11-27 21:27:15 +0100

    HW 05/11

---
+++ HW 05.11/script.js
@@ -0,0 +1,105 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinaryTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      this.insertNode(this.root, newNode);
+    }
+  }
+
+  insertNode(node, newNode) {
+    if (newNode.value < node.value) {
+      if (node.left === null) {
+        node.left = newNode;
+      } else {
+        this.insertNode(node.left, newNode);
+      }
+    } else {
+      if (node.right === null) {
+        node.right = newNode;
+      } else {
+        this.insertNode(node.right, newNode);
+      }
+    }
+  }
+
+  inorder(node) {
+    if (node !== null) {
+      this.inorder(node.left);
+      console.log(node.value);
+      this.inorder(node.right);
+    }
+  }
+
+  delete(value) {
+    this.root = this.deleteNode(this.root, value);
+  }
+
+  deleteNode(node, value) {
+    if (node === null) {
+      return null;
+    }
+
+    if (value < node.value) {
+      node.left = this.deleteNode(node.left, value);
+      return node;
+    } else if (value > node.value) {
+      node.right = this.deleteNode(node.right, value);
+      return node;
+    } else {
+      if (node.left === null && node.right === null) {
+        node = null;
+        return node;
+      }
+
+      if (node.left === null) {
+        node = node.right;
+        return node;
+      } else if (node.right === null) {
+        node = node.left;
+        return node;
+      }
+
+      const minNode = this.findMinNode(node.right);
+      node.value = minNode.value;
+      node.right = this.deleteNode(node.right, minNode.value);
+      return node;
+    }
+  }
+
+  findMinNode(node) {
+    while (node.left !== null) {
+      node = node.left;
+    }
+    return node;
+  }
+}
+
+// Пример использования
+const tree = new BinaryTree();
+tree.insert(13);
+tree.insert(5);
+tree.insert(15);
+tree.insert(6);
+tree.insert(3);
+
+console.log("Inorder traversal до удаления:");
+tree.inorder(tree.root);
+
+tree.delete(6);
+
+console.log("Inorder traversal после удаления 6:");
+tree.inorder(tree.root);
